931b2e471255f4f80088fae8d6d1781d52ce2159
Sarah Busson <sarahbussonsb@gmail.com>
2020-11-10 18:05:33 +0100

    Step 2

--- lyrics_from_spotify.py
+++ lyrics_from_spotify.py
@@ -3,8 +3,7 @@ Step 1
 Connect to Spotify and GET Player
 
 Step 2
-API to Genius 
-OR scrap the lyrics ?
+Scrap the lyrics (on google)
 
 Step 3
 Connect to WhatsApp and send the lyrics
@@ -31,18 +30,32 @@ class GetLyrics:
     def __init__(self):
         pass
 
-# Step 1 : Get the song currently playing on Spotify
+# Step 1: Get the song currently playing on Spotify
     # Playlist ID:
     songs_to_sing = '7tPSBvTBEh7qJ4X192u9uP'
 
     def get_playing_song(self):
+        #SPOTIFY REQUEST
         current_song = sp.currently_playing(market='FR', additional_types='track')
-        return current_song #json format
-        
+        flat_df = pd.io.json.json_normalize(current_song)
+        #TRACK NAME
+        track_name = flat_df['item.name'][0]
+        #TRACK ARTIST (only the main artist)
+        track_artist = flat_df['item.artists'][0][0]['name']
+        #PLAYLIST
+        playlist = re.split(':', flat_df['context.uri'][0])[2]
+
+        return track_name, track_artist, playlist
+    
+# Step 2: Get the song lyrics from web scrapping google lyrics
+
+
+
+
 """ 
-Now make sure that the playlist match, 
-then collect title and artist 
-then use it in Genius API OR scrap the genius website
+Now make sure that the playlist match, WAIT
+then collect title and artist           V
+then scrap the google lyrics            
 then collect the lyrics or send the link ??
 then connect to WhatsApp
 then send lyrics or link via text
